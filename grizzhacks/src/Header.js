f141670bdade83845cebe708b0c2f910d8276523
Zack Lain <zacharytlain@gmail.com>
2024-03-09 18:25:38 -0500

    Refactor App component and add Header and Footer components

---
+++ grizzhacks/src/Header.js
@@ -0,0 +1,25 @@
+function header() {
+  return (
+    <header>
+        <div>
+        <h1>GrizzHacks 24</h1>
+        </div>
+        <div>
+            <nav>
+                <ul>
+                    <li>Bio Informatics</li>
+                    <li>Cyber Security</li>
+                    <li>Computational Intelligence</li>
+                    <li>Decision Optimization</li>
+                    <li>Game Development</li>
+                    <li>Mobile Applications</li>
+                    <li>Systems Admin</li>
+                    <li>Web Development</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+  );
+}
+
+export default header;
